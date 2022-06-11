d7209f1a775e04007f560e8cec3ac0810c069823
rootsong8774 <95426072+rootsong8774@users.noreply.github.com>
2022-06-11 16:51:01 +0900

    Template Method Pattern 적용

---
+++ src/main/java/hello/springadvanced/app/v4/OrderRepositoryV4.java
@@ -0,0 +1,42 @@
+package hello.springadvanced.app.v4;
+
+import hello.springadvanced.trace.TraceStatus;
+import hello.springadvanced.trace.logtrace.LogTrace;
+import hello.springadvanced.trace.template.AbstractTemplate;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@RequiredArgsConstructor
+@Repository
+public class OrderRepositoryV4 {
+    
+    private final LogTrace trace;
+    
+    
+    
+    public void save(String itemId) {
+    
+        AbstractTemplate<Void> template = new AbstractTemplate<>(trace) {
+            @Override
+            protected Void call() {
+                //저장 로직
+                if(itemId.equals("ex")){
+                    throw new IllegalStateException("예외 발생!");
+                }
+                sleep(1000);
+                return null;
+            }
+        };
+        template.execute("OrderRepository.save()");
+        
+    }
+    
+    private void sleep(int millis) {
+    
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
